28f10df43ed53bcca31295a2a8f6970ebe5350fc
Kaveenxz <kaveenhansithx@gmail.com>
2024-02-08 21:09:13 +0530

    add api to get all countries to set register module

--- src/app/page/register/register.component.ts
+++ src/app/page/register/register.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule, HttpErrorResponse } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, FormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+
+export class RegisterComponent implements OnInit{
+
+  private http;
+  public countryList:any;
+
+  constructor(private httpClient:HttpClient){
+    this.http = httpClient
+  }
+  ngOnInit(): void {
+      this.loadCountries()
+  }
+
+  loadCountries(){
+    let api = "https://restcountries.com/v3.1/all";
+    this.http.get(api).subscribe(res =>{
+      console.log(res);
+      
+    })
+  }
 
 }
