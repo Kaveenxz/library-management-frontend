0917dc818df057e4509c50608cc5b5dca0be4271
Kaveenxz <kaveenhansithx@gmail.com>
2024-02-08 20:57:14 +0530

    add sign-up form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './page/login/login.component';
 import { ViewAllBooksComponent } from './page/view-all-books/view-all-books.component';
+import { RegisterComponent } from './page/register/register.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path:"view-all-book",
         component:ViewAllBooksComponent
+    },
+    {
+        path:"sing-up",
+        component:RegisterComponent
     }
 ];
