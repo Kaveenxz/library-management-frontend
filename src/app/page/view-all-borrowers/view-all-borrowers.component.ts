230b510617579f4763483b87b853b03f2dc7cb0f
Kaveenxz <kaveenhansithx@gmail.com>
2024-02-11 21:07:36 +0530

    make borrowers component

---
+++ src/app/page/view-all-borrowers/view-all-borrowers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-borrowers',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-all-borrowers.component.html',
+  styleUrl: './view-all-borrowers.component.css'
+})
+export class ViewAllBorrowersComponent {
+
+}
