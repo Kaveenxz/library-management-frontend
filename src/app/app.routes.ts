b87f96b7855a6eeb7757d45fb9390a70270ba364
Kaveenxz <kaveenhansithx@gmail.com>
2024-02-16 21:40:44 +0530

    updated routing to borowes section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './page/login/login.component';
 import { ViewAllBooksComponent } from './page/view-all-books/view-all-books.component';
 import { RegisterComponent } from './page/register/register.component';
+import { ViewAllBorrowersComponent } from './page/view-all-borrowers/view-all-borrowers.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,10 @@ export const routes: Routes = [
     {
         path:"sing-up",
         component:RegisterComponent
+    },
+    {
+        path:"view-all-Borrowers",
+        component:ViewAllBorrowersComponent
     }
+
 ];
