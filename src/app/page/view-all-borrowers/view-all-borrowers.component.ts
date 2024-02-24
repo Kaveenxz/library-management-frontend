94cd7f8a296f2eb6175010e4aab30cb9c65ee093
Kaveenxz <kaveenhansithx@gmail.com>
2024-02-24 20:55:05 +0530

    ts file with crud

--- src/app/page/view-all-borrowers/view-all-borrowers.component.ts
+++ src/app/page/view-all-borrowers/view-all-borrowers.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import  Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-view-all-borrowers',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, FormsModule, CommonModule],
   templateUrl: './view-all-borrowers.component.html',
   styleUrl: './view-all-borrowers.component.css'
 })
-export class ViewAllBorrowersComponent {
+export class ViewAllBorrowersComponent implements OnInit{
+  private http;
+  public borroweList:any = {};
+  public selectedBorrower:any;
 
+  constructor(private httpClient:HttpClient){
+    this.http = httpClient;
+  }
+  ngOnInit(): void {
+      this.loadBorrowers();
+  }
+  loadBorrowers(){
+    this.http.get('http://localhost:8081/borrower/get').subscribe((data) =>{
+      this.borroweList=data;
+      console.log(this.borroweList);
+    })
+  }
+
+  deleteBorrowers() {
+    let api = "http://localhost:8080/book/"+this.selectedBorrower.id;
+    this.http.delete(api, { responseType: 'text' }).subscribe(
+      (response: string) => {
+        this.loadBorrowers();
+        Swal.fire({
+          title: "Good job!",
+          text: `${this.selectedBorrower.title} book is deleted`,
+          icon: "success"
+        });
+        this.selectedBorrower = null;
+
+      }
+    )
+  }
+  
+  setSelecteBorrower(book:any){
+    this.selectedBorrower = book;
+  }
+
+  saveBook(){
+    let postApi = "http://localhost:8080/borrower/add"
+    this.http.post(postApi, this.selectedBorrower).subscribe(data =>{
+      console.log("saved!");
+      this.loadBorrowers();
+      Swal.fire({
+        title: "Good job! Updated book",
+        text: `${this.selectedBorrower.title} book is Updated`,
+        icon: "success"
+      });
+      this.selectedBorrower={};
+    })
+  }
 }
