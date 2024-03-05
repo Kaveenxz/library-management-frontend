ffdbb5ffffcec809d9fd680f813c51dfb4eee895
Kaveenxz <kaveenhansithx@gmail.com>
2024-03-05 22:30:21 +0530

    update user change

--- src/app/page/view-all-borrowers/view-all-borrowers.component.ts
+++ src/app/page/view-all-borrowers/view-all-borrowers.component.ts
@@ -3,14 +3,15 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import Swal from 'sweetalert2';
+import { NavComponent } from "../../common/nav/nav.component";
 
 
 @Component({
-  selector: 'app-view-all-borrowers',
-  standalone: true,
-  imports: [HttpClientModule, FormsModule, CommonModule],
-  templateUrl: './view-all-borrowers.component.html',
-  styleUrl: './view-all-borrowers.component.css'
+    selector: 'app-view-all-borrowers',
+    standalone: true,
+    templateUrl: './view-all-borrowers.component.html',
+    styleUrl: './view-all-borrowers.component.css',
+    imports: [HttpClientModule, FormsModule, CommonModule, NavComponent]
 })
 export class ViewAllBorrowersComponent implements OnInit {
   public userList: any = {};
@@ -33,19 +34,45 @@ export class ViewAllBorrowersComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadUsers();
-  }
+}
 
   loadUsers() {
-    this.http.get("http://localhost:8081/user/get-all").subscribe((res: any) => {
+    this.http.get(this.baseUrl+"/user/get-all").subscribe((res: any) => {
       console.log(res);
       this.userList = res;
     })
   }
 
   deleteUser() { 
-    let api = ""
-  }
-  saveUser() { }
+    this.http.delete(this.baseUrl+"/user/delete/"+this.selectedUser.id, {responseType:'text'}).subscribe((res:any) =>{
+      console.log(res);
+      this.loadUsers()
+      Swal.fire({
+        title: "Good job!",
+        text: `${this.selectedUser.userName} user is deleted`,
+        icon: "success"
+      });
+      this.selectedUser = null;
 
+      
+    })
+  }
+  saveUser() {
+    this.http.post("http://localhost:8081/user/add-user/",this.selectedUser).subscribe((res:any)=>{
+      this.loadUsers();
+      Swal.fire({
+        title: "Good job! Updated user",
+        text: `${this.selectedUser.userName} user is Updated`,
+        icon: "success"
+      });
+      this.selectedUser={};
+    })
+   }
+
+  setSelectedUser(user:any){
+    this.selectedUser=user;
+    console.log(user);
+    
+  }
 
 }
