188d4a7afe5e8094a1f5d0babbb73d2abf9ca599
Kaveenxz <kaveenhansithx@gmail.com>
2024-03-09 17:34:29 +0530

    make the login functional

--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,13 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, HttpClientModule, FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  loginObj:any ={
+    "email": "",
+    "passWord": ""
+  }
+
+  constructor(private http: HttpClient, private router:Router){}
+
+  login(){
+    this.http.post("http://localhost:8081/login/request-login", this.login).subscribe((res:any)=>{
+      if(res==true){
+        this.router.navigate(["/view-all-book"])
+      }else{
+        alert("Incorrect Details!")
+      }
+    })    
+  }
 }
